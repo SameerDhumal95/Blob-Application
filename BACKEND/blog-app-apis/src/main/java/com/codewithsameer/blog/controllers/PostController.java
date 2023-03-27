09ad2f32c906e2ceba0d2523a6f974e14dfcaef9
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2023-03-27 09:31:29 +0530

    best practice used AppConstants class to store hardcord values

--- BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/controllers/PostController.java
+++ BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithsameer.blog.config.AppConstants;
 import com.codewithsameer.blog.entities.Post;
 import com.codewithsameer.blog.payloads.ApiResponse;
 import com.codewithsameer.blog.payloads.PostDto;
@@ -69,10 +70,10 @@ public class PostController {
 	
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(//pageNumber will starts from 0
-	@RequestParam(value = "pageNumber",defaultValue = "0", required =  false) Integer pageNumber,
-	@RequestParam(value="pageSize",defaultValue ="10", required = false) Integer pageSize,
-	@RequestParam(value = "sortBy" , defaultValue ="postId",required = false)String sortBy,
-	@RequestParam(value = "sortDir" , defaultValue ="asc",required = false)String sortDir)
+	@RequestParam(value = "pageNumber",defaultValue = AppConstants.PAGE_NUMBER, required =  false) Integer pageNumber,
+	@RequestParam(value="pageSize",defaultValue =AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+	@RequestParam(value = "sortBy" , defaultValue =AppConstants.SORT_BY,required = false)String sortBy,
+	@RequestParam(value = "sortDir" , defaultValue =AppConstants.SORT_DIR,required = false)String sortDir)
 	{
 	    PostResponse postResponse = this.postService.getAllPost(pageNumber,pageSize,sortBy,sortDir);
 		
