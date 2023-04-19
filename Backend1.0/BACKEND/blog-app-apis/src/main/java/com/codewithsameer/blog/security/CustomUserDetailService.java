601c514015423110a9de8b820cf6f3d2c2aa4812
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2023-04-19 20:19:40 +0530

    Password Secured using Password Encoder

--- Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/security/CustomUserDetailService.java
+++ Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/security/CustomUserDetailService.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.codewithsameer.blog.entities.User;
 import com.codewithsameer.blog.exceptions.ResourceNotFoundException;
 import com.codewithsameer.blog.repositories.UserRepo;
 
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
 	@Autowired
