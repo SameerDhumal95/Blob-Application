36813f8a3adb1031f976bd3d6858904524ca3b39
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2023-04-19 20:07:55 +0530

    Security beans Dao added

--- Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/repositories/UserRepo.java
+++ Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/repositories/UserRepo.java
@@ -1,9 +1,13 @@
 package com.codewithsameer.blog.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codewithsameer.blog.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
 
+	Optional<User> findByEmail(String email);
+	
 }
