1f6f61dccd71e26a049dd0441252d98a3387696a
SameerDhumal <90055461+SameerDhumal@users.noreply.github.com>
2023-04-20 14:22:19 +0530

    JWT Authentication added

--- Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/config/SecurityConfig.java
+++ Backend1.0/BACKEND/blog-app-apis/src/main/java/com/codewithsameer/blog/config/SecurityConfig.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.codewithsameer.blog.security.CustomUserDetailService;
+import com.codewithsameer.blog.security.JwtAuthenticationEntryPoint;
+import com.codewithsameer.blog.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +26,11 @@ public class SecurityConfig {
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
 	
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
 	
 	  @Bean
 	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,10 +38,18 @@ public class SecurityConfig {
 	        .csrf()
 	        .disable()
 	        .authorizeHttpRequests()
+	       
 	        .anyRequest()
 	        .authenticated()
 	        .and()
-	        .httpBasic();
+	        .exceptionHandling()
+	        .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+	        .and()
+	        .sessionManagement()
+	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	        
+	        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	        
 	        return http.build();
 	    }
 	
@@ -51,9 +69,9 @@ public class SecurityConfig {
 		  return provider;
 	  }
 	  
-//	  @Bean
-//	  public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception
-//	  {
-//		  return configuration.getAuthenticationManager();
-//	  }
+	  @Bean
+	  public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception
+	  {
+		  return configuration.getAuthenticationManager();
+	  }
 }
